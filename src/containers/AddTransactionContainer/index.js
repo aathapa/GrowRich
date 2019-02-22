8a944ddaa0a5eb8e880b2338901d496fd856d36c
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2019-02-23 00:59:30 +0545

    update transaction data and other miscellaneous task completed

--- src/containers/AddTransactionContainer/index.js
+++ src/containers/AddTransactionContainer/index.js
@@ -5,11 +5,13 @@ import {
   Animated,
   Dimensions,
   TouchableOpacity,
+  BackHandler
 } from 'react-native'
 import { Navigation } from 'react-native-navigation'
 import { Button } from 'react-native-elements'
 import DateTimePicker from 'react-native-modal-datetime-picker';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
+import IonIcons from 'react-native-vector-icons/Ionicons'
+
 import { openDatabase } from 'react-native-sqlite-storage'
 import uuid from 'uuid/v4'
 import Modal from 'react-native-modal'
@@ -17,15 +19,14 @@ import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view
 import { connect } from 'react-redux'
 
 import { CategoryListing, Form, TopBar } from 'component'
-
 import { formatDate } from 'helper'
-
-import { insertDataInTransaction, fetchCategoryData } from '../../database'
-import { Images, Icons } from 'globalData'
+import { editTransaction } from '../../redux/actions'
+import { insertDataInTransaction, fetchCategoryData, updateTransactionData } from '../../database'
+import { Images, Icons, Dimension } from 'globalData'
 import styles from './styles'
 
 
-const { height } = Dimensions.get('window')
+const { height, width } = Dimensions.get('window')
 const db = openDatabase({ name: 'Expense.db' })
 
 const colors = ['#F25365', '#FFCE6A', '#FF6C58', '#43C1E4', '#AD94E5', '#569EE6']
@@ -33,28 +34,56 @@ const colors = ['#F25365', '#FFCE6A', '#FF6C58', '#43C1E4', '#AD94E5', '#569EE6'
 
 class AddTransactionContainer extends Component {
 
+  initialState = {
+    amount: '',
+    memo: '',
+    selectedCategoryItem: null,
+    open: false
+  }
   state = {
-    open: false,
+    ...this.initialState,
+    isCategoryModalOpen: false,
     currentActiveMenu: 'Expense',
     currentActiveMenuData: [],
-    selectedCategoryItem: null,
     isDateTimePickerVisible: false,
     currentDate: formatDate(),
-    amount: '',
-    memo: ''
   }
 
   categoryHeightValue = new Animated.Value(0)
-  toastAnimationValue = new Animated.Value(0)
 
   componentDidMount() {
     Navigation.events().bindComponent(this)
+
   }
 
   componentDidAppear() {
     const { currentActiveMenu } = this.state
+    this.backHandler = BackHandler.addEventListener('hardwareBackPress', () => {
+      if (this.state.open) {
+        this.animatedCategoryHeight()
+        return true
+      }
+      BackHandler.exitApp()
+    })
     this.fetchData(type = currentActiveMenu)
+    if (this.checkEditData()) {
+      this.animatedCategoryHeight()
+      this.setState({
+        amount: this.props.selectedTransactionItemData.amount.toString(),
+        memo: this.props.selectedTransactionItemData.memo,
+        selectedCategoryItem: this.props.selectedTransactionItemData.category,
+        currentActiveMenu: this.props.selectedTransactionItemData.transaction_type
 
+      })
+    }
+
+  }
+
+  componentDidDisappear() {
+    this.backHandler.remove()
+    this.props.editTransaction({})
+    this.setState({ ...this.initialState })
+    this.categoryHeightValue.setValue(0)
   }
 
   async fetchData(type) {
@@ -67,7 +96,7 @@ class AddTransactionContainer extends Component {
     }
   }
 
-  async insertData() {
+  insertData = async () => {
     const { amount, memo, currentDate, currentActiveMenu, selectedCategoryItem } = this.state
 
     if (amount.length === 0) {
@@ -75,98 +104,141 @@ class AddTransactionContainer extends Component {
       return
     }
 
-    const vars = [uuid(), currentActiveMenu, selectedCategoryItem, currentDate, amount, memo, colors[Math.floor(Math.random() * colors.length)], Date.now()]
-    try {
-      await insertDataInTransaction(db, vars)
+    const insertVars = [uuid(), currentActiveMenu, selectedCategoryItem, currentDate, amount, memo, colors[Math.floor(Math.random() * colors.length)], Date.now()]
+    const updateVars = [currentActiveMenu, selectedCategoryItem, currentDate, amount, memo, this.props.selectedTransactionItemData.id]
+    if (this.checkEditData()) {
+      updateTransactionData(db, updateVars)
     }
-    catch (e) {
-      alert(e)
+    else {
+      try {
+        await insertDataInTransaction(db, insertVars)
+      }
+      catch (e) {
+        alert(e)
+      }
     }
+
     this.setState({ amount: '', memo: '' })
+    setTimeout(() => {
+      this.switchToTab(0)
+    }, 200);
+
+  }
+
+  switchToTab(tabIndex) {
+    Navigation.mergeOptions(this.props.componentId, {
+      bottomTabs: {
+        currentTabIndex: tabIndex
+      }
+    })
   }
 
   onTopbarHeadingPressed = () => {
     this.setState(prevstate => ({
-      open: !prevstate.open
+      isCategoryModalOpen: !prevstate.isCategoryModalOpen
     }))
-  
+
   }
 
   onClickMenu(type) {
-    this.setState({ currentActiveMenu: type, open: false, }, () => {
+    this.setState({ currentActiveMenu: type, isCategoryModalOpen: false, open: false }, () => {
       this.fetchData(this.state.currentActiveMenu)
       this.categoryHeightValue.setValue(0)
     })
+  }
+
+  onPressCategory = () => {
+    this.categoryHeightValue.setValue(0)
+    this.setState({ open: false })
   }
 
   onChangeText = (type, value) => {
     this.setState({ [type]: value })
   }
 
-  animatedCategoryHeight() {
+  animatedCategoryHeight = () => {
+    const toValue = this.state.open ? 0 : 1
     Animated.timing(this.categoryHeightValue, {
-      toValue: 1,
-      duration: 100,
-    }).start()
+      toValue,
+      duration: 200,
+      useNativeDriver: true
+    }).start(finished => {
+      if (finished) this.setState(prevSate => ({ open: !prevSate.open }))
+
+    })
+  }
+
+  checkEditData() {
+    return Object.keys(this.props.selectedTransactionItemData).length > 0
   }
 
   render() {
-    const { amount, memo, currentDate, selectedCategoryItem } = this.state;
-    const categoryHeight = this.categoryHeightValue.interpolate({
+    const { amount, memo, currentDate, selectedCategoryItem, open,
+      currentActiveMenu, currentActiveMenuData, isCategoryModalOpen, isDateTimePickerVisible } = this.state;
+    const translateX = this.categoryHeightValue.interpolate({
       inputRange: [0, 1],
-      outputRange: [height, 0]
+      outputRange: [0, -height]
     })
 
-    console.log(this.props.data)
+    const formTranslateX = this.categoryHeightValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: [width, 0]
+    })
+
+    const isActive = (value) => currentActiveMenu === value
 
     return (
-      <View style={{ height }}>
+
+      <View style={{ height }} >
         <TopBar
           onPress={this.onTopbarHeadingPressed}
           iconName={Icons.IonIcons.filter}
-          title={this.state.currentActiveMenu}
+          title={currentActiveMenu}
+          renderBackButton={open}
+          backPress={this.animatedCategoryHeight}
         />
-
-        <Animated.View style={{ height: categoryHeight }}>
-          <CategoryListing
-            onPress={(item) => {
-              this.setState({ selectedCategoryItem: item })
-              this.animatedCategoryHeight()
-            }}
-            data={this.state.currentActiveMenuData}
-            images={Images}
-          />
-        </Animated.View>
-
         <CategoryForm
+          formTranslateX={formTranslateX}
           memo={memo}
           amount={amount}
           selectedCategoryItem={selectedCategoryItem}
           dateTimePickerVisible={() => this.setState({ isDateTimePickerVisible: true })}
           currentDate={currentDate}
           onChangeText={this.onChangeText}
-          onButtonPress={() => this.insertData()}
-          onPress={() => this.categoryHeightValue.setValue(0)}
+          onButtonPress={this.insertData}
+          onPress={this.onPressCategory}
+          checkEditData={this.checkEditData.bind(this)}
         />
+
+        <Animated.View style={{ transform: [{ translateX }], position: 'absolute', top: Dimension.TABBAR_HEIGHT, right: 0, left: 0 }}>
+          <CategoryListing
+            onPress={(item) => {
+              this.setState({ selectedCategoryItem: item })
+              this.animatedCategoryHeight()
+            }}
+            data={currentActiveMenuData}
+            images={Images}
+          />
+        </Animated.View>
+
+
         <CategoryModal
-          open={this.state.open}
-          onExpensePressed={() => this.onClickMenu('Expense')}
-          onIncomePressed={() => this.onClickMenu('Income')}
-          onBackdropPress={() => this.setState({ open: false })}
+          isCategoryModalOpen={isCategoryModalOpen}
+          onExpensePressed={this.onClickMenu.bind(this, 'Expense')}
+          onIncomePressed={this.onClickMenu.bind(this, 'Income')}
+          onBackdropPress={() => this.setState({ isCategoryModalOpen: false })}
+          isActive={isActive}
+          currentActiveMenu={currentActiveMenu}
         />
 
         <DateTimePicker
-          isVisible={this.state.isDateTimePickerVisible}
+          isVisible={isDateTimePickerVisible}
           onConfirm={(date) => this.setState({ isDateTimePickerVisible: false, currentDate: formatDate(date.getFullYear(), date.getMonth() + 1, date.getDate()) })
           }
           onCancel={() => this.setState({ isDateTimePickerVisible: false })}
         />
-        <Animated.View
-          style={{ height: 80, backgroundColor: '#000', position: 'absolute', top: 30, right: 20, left: 20, transform: [{ translateY: 400 }], opacity: 0 }}
-        >
-          <Text>Select New Category</Text>
-        </Animated.View>
       </View >
+
     )
   }
 
@@ -188,14 +260,17 @@ function CategoryForm({
   currentDate,
   dateTimePickerVisible,
   onButtonPress,
-  onPress
+  onPress,
+  formTranslateX,
+  checkEditData
 }) {
   return (
     <KeyboardAwareScrollView
-      extraScrollHeight={50}
       enableOnAndroid
+      keyboardShouldPersistTaps="always"
+      extraScrollHeight={50}
     >
-      <View style={{ marginHorizontal: 15, marginTop: 15 }}>
+      <Animated.View style={{ marginHorizontal: 15, marginTop: 15, transform: [{ translateX: formTranslateX }] }}>
         <View style={{ borderBottomWidth: 0.7, borderBottomColor: '#5B3BB4' }}>
           <View>
             <Text style={{ color: '#757575' }}>Category</Text>
@@ -204,7 +279,7 @@ function CategoryForm({
             <TouchableOpacity onPress={onPress}>
               <View style={{ flexDirection: 'row' }}>
                 <View style={{ flex: 1 }}>
-                  <SimpleLineIcons name="grid" size={20} color="#616161" />
+                  <IonIcons name={Icons.IonIcons.grid} size={20} color="#616161" />
                 </View>
                 <View style={{ flex: 10 }}>
                   <Text style={{ fontSize: 17, color: '#424242' }}>{selectedCategoryItem}</Text>
@@ -223,7 +298,7 @@ function CategoryForm({
             <TouchableOpacity onPress={dateTimePickerVisible}>
               <View style={{ flexDirection: 'row' }}>
                 <View style={{ flex: 1 }}>
-                  <SimpleLineIcons name="calendar" size={20} color="#616161" />
+                  <IonIcons name={Icons.IonIcons.calender} size={20} color="#616161" />
                 </View>
                 <View style={{ flex: 10 }}>
                   <Text style={{ fontSize: 17, color: '#424242' }}>{currentDate}</Text>
@@ -254,12 +329,12 @@ function CategoryForm({
 
         <View style={{ marginTop: 20 }}>
           <Button
-            title="Add"
+            title={checkEditData() ? 'Update' : 'Add'}
             backgroundColor="#9DD377"
             onPress={onButtonPress}
           />
         </View>
-      </View>
+      </Animated.View>
     </KeyboardAwareScrollView>
   )
 }
@@ -273,14 +348,16 @@ function CategoryForm({
  */
 
 function CategoryModal({
-  open,
+  isCategoryModalOpen,
   onExpensePressed,
   onIncomePressed,
-  onBackdropPress
+  onBackdropPress,
+  isActive
 }) {
+  const icon = <IonIcons name={Icons.IonIcons.check} size={30} color="#fff" />
   return (
     <Modal
-      isVisible={open}
+      isVisible={isCategoryModalOpen}
       style={{ justifyContent: 'flex-end', margin: 0 }}
       animationInTiming={50}
       onBackdropPress={onBackdropPress}
@@ -288,17 +365,28 @@ function CategoryModal({
     >
       <View style={styles.categoryModalview}>
         <TouchableOpacity
-          style={{ flex: 1, }}
+          style={{ flex: 1, flexDirection: 'row' }}
           onPress={onExpensePressed}
         >
-          <Text style={{ color: '#fff', fontSize: 18 }}>Expense</Text>
+          <View style={{ flex: 1 }}>
+            <Text style={{ color: '#fff', fontSize: 18 }}>Expense</Text>
+          </View>
+          <View style={{ flex: 2 }}>
+            {isActive('Expense') && icon}
+          </View>
+
         </TouchableOpacity>
 
         <TouchableOpacity
           onPress={onIncomePressed}
-          style={{ flex: 1, }}
+          style={{ flex: 1, flexDirection: 'row' }}
         >
-          <Text style={{ color: '#fff', fontSize: 18 }}>Income</Text>
+          <View style={{ flex: 1 }}>
+            <Text style={{ color: '#fff', fontSize: 18 }}>Income</Text>
+          </View>
+          <View style={{ flex: 2 }}>
+            {isActive('Income') && icon}
+          </View>
         </TouchableOpacity>
       </View>
 
@@ -306,4 +394,4 @@ function CategoryModal({
   )
 }
 
-export default connect(state => ({ data: state.transaction.selectedItemData }))(AddTransactionContainer)
+export default connect(state => ({ selectedTransactionItemData: state.transaction.selectedItemData }), { editTransaction })(AddTransactionContainer)
