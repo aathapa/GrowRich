cb1e6e9ee524d782f35653d82cd2f72db3bada1f
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2019-02-23 00:46:17 +0545

    icons added

--- src/globalData/Icon.js
+++ src/globalData/Icon.js
@@ -7,11 +7,19 @@ export const Icons = {
         filter: 'ios-funnel',
         arrowBack: 'ios-arrow-back',
         arrowForward: 'ios-arrow-forward',
+        grid: 'ios-grid',
+        calender: 'ios-calendar',
+        check: 'ios-checkmark',
+        back: 'ios-arrow-round-back'
       },
       android: {
         filter: 'md-funnel',
         arrowBack: 'md-arrow-back',
         arrowForward: 'md-arrow-forward',
+        grid: 'md-grid',
+        calender: 'md-calendar',
+        check: 'md-checkmark',
+        back: 'md-arrow-back'
       }
     })
   },
